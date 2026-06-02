c6feed82ddb6075506b756541518ca38bbf48767
obyead-design <charlie@Jarvis.localdomain>
2026-06-03 02:34:16 +0600

    g

--- public/index.php
+++ public/index.php
@@ -1,14 +1,9 @@
 <?php
 
 use App\Kernel;
-use Symfony\Component\Dotenv\Dotenv;
 use Symfony\Component\HttpFoundation\Request;
 
 require dirname(__DIR__) . '/vendor/autoload.php';
-
-if (file_exists(dirname(__DIR__) . '/.env')) {
-    (new Dotenv())->bootEnv(dirname(__DIR__) . '/.env');
-}
 
 $kernel = new Kernel($_SERVER['APP_ENV'] ?? 'prod', (bool) ($_SERVER['APP_DEBUG'] ?? false));
 
